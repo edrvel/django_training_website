06735be25ef2690134857eeb60f84f16e3225a60
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-08-25 19:01:41 +0500

    Третий коммит, создание crm app и его модели базы данных (models.py), миграция в PostgreSQL

--- landing_page_website/settings.py
+++ landing_page_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'landing_page_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_training_project',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
