54cc19189e6ce7272ec2b9aa243e05e8ba2284c3
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-08-26 15:56:26 +0500

    Пятый коммит, добавление обработчика формы

--- crm/models.py
+++ crm/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Order(models.Model):
-    order_id = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    order_id = models.AutoField(primary_key=True)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата')
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name='Телефон')
 
     def __str__(self):
-        return f'Заказ {self.id}'
+        return f'Заказ {self.order_id}'
     
     class Meta:
         verbose_name = 'Заказ'
