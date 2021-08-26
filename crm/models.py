9362944162646aaec0c6b09ab2eecdbda5bdda22
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-08-26 13:11:01 +0500

    Четвертый коммит, настройка админ-панели, регистрация crm, шаблонные теги

--- crm/models.py
+++ crm/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Order(models.Model):
-    order_id = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_id = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+
+    def __str__(self):
+        return f'Заказ {self.id}'
+    
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
